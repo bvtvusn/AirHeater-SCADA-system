7a3024951404b924307c49048ead9b3cdc8b3e2a
bvtv <57758767+bvtvusn@users.noreply.github.com>
2024-03-23 08:36:18 +0100

    Create secrets.h

---
+++ arduinoSketch/secrets.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID "mySSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "myPass"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1212121			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "XYZ"   // replace XYZ with your channel write API Key
